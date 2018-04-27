7d97fab672b1ac7ba0f82db3ba1fec8d113b6f08
SekarAyu <ayusoomvlak@gmail.com>
2018-04-27 18:16:25 +0700

    sprint3

---
+++ application/views/form_v.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Input Data</title>
+</head>
+<body>
+<center>
+	<form action="http://localhost/codeigniter/index.php/user/add" method="POST">
+	<table>
+		<tr><td>Username</td> <td>:</td><td><input name="username" value=""></td></tr>
+		<tr><td>Password</td><td>:</td> <td><input name="password" type="password" value=""></td></tr>
+		<tr><td>Fullname</td> <td>:</td><td><input name="fullname" value=""></td></tr>
+		<tr><td>Level</td> <td>:</td><td><input name="level" value=""></td></tr>
+		<tr><td><input name="tombol" type="submit" value="simpan"></td></tr>
+	</table>
+	</form>
+</center>
+</body>
+</html>
