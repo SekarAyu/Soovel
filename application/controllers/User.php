cd49d40f7d538217f3d36ed13314586042dd9611
SekarAyu <ayusoomvlak@gmail.com>
2018-05-03 16:10:43 +0700

    4

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -1,14 +1,19 @@
 <?php
 class User extends CI_Controller{
+	function __construct()
+	{
+		parent :: __construct();
+		$this->load->model("user_m");
+	}
 	public function form()
 	{
 		$this->load->view("form_v");
 	}
-	public function index()
-	{
-		$this->load->view("welcome_message");	}
 	public function add()
 	{
-		echo $this->input->post("username");
+
+		$data = array('username' => $this->input->post("username"), 'password' => $this->input->post("password"), 'fullname' => $this->input->post("fullname"), 'level' => $this->input->post("level") );
+		var_dump($data);
+		$this->user_m->add($data);
 	}
 }
--- application/views/form_v.php
+++ application/views/form_v.php
@@ -10,7 +10,10 @@
 		<tr><td>Username</td> <td>:</td><td><input name="username" value=""></td></tr>
 		<tr><td>Password</td><td>:</td> <td><input name="password" type="password" value=""></td></tr>
 		<tr><td>Fullname</td> <td>:</td><td><input name="fullname" value=""></td></tr>
-		<tr><td>Level</td> <td>:</td><td><input name="level" value=""></td></tr>
+		<tr><td>Level</td> <td>:</td><td><select name="level">
+			<option value="admin">Admin</option>
+			<option value="user">User</option>
+		</select></td></tr>
 		<tr><td><input name="tombol" type="submit" value="simpan"></td></tr>
 	</table>
 	</form>
